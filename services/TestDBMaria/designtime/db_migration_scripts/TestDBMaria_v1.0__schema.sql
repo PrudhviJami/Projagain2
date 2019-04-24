a4ca568130fc5334dbe6fd773cd0d684b7f26978
Test Userp <TestUserp@fico.com>
2019-04-24 14:44:52 +0000

    push02 New files added - [TestDBMaria,hrdb,data] Files modified - [Main,securityService,WEB-INF,profiles,pom.xml,app.variables.json]

---
+++ services/TestDBMaria/designtime/db_migration_scripts/TestDBMaria_v1.0__schema.sql
@@ -0,0 +1,11 @@
+-- MIGRATION SCRIPT
+-- Database Type: DBType{type='mysql', urlScheme='jdbc:', dialect='org.hibernate.dialect.MySQLDialect', driverClass='com.mysql.jdbc.Driver'}
+-- App Name: Projagain2
+-- Database: TestDBMaria
+-- Created At: 24-Apr-2019 14:41:11.UTC
+-- Script Version: 1.0
+-- -----------------------------------------------------------------
+CREATE TABLE `TABLE1` (`ID` INT NOT NULL AUTO_INCREMENT,
+`COLUMN2` VARCHAR(255) NULL,
+`COLUMN3` VARCHAR(255) NULL,
+PRIMARY KEY(`ID`));
---
+++ services/TestDBMaria/designtime/db_migration_scripts/TestDBMaria_v1.1__user_data.sql
@@ -0,0 +1,8 @@
+-- MIGRATION SCRIPT
+-- Database Type: DBType{type='mysql', urlScheme='jdbc:', dialect='org.hibernate.dialect.MySQLDialect', driverClass='com.mysql.jdbc.Driver'}
+-- App Name: Projagain2
+-- Database: TestDBMaria
+-- Created At: 24-Apr-2019 14:41:11.UTC
+-- Script Version: 1.1
+-- -----------------------------------------------------------------
+-- USER SQL SCRIPT
